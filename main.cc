7e6b0d98c0721e1d13df0f570674d4d4e5468026
imlishant <lishantsahu@gmail.com>
2026-07-15 17:53:18 +0530

    removed comment from main, new ray file

--- main.cc
+++ main.cc
@@ -16,20 +16,10 @@ int main() {
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
         for (int i = 0; i < image_width; i++) {
-            /*
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
-            */
 
             auto pixels = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
             write_color(std::cout, pixels);
+
         }
     }
 
