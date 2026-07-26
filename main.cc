02e3521f67cc3f5e292849b0c06a2831289d65e3
imlishant <lishantsahu@gmail.com>
2026-07-26 21:10:14 +0530

    add camera.h, edit main.cc to make readable

--- main.cc
+++ main.cc
@@ -1,85 +1,27 @@
 #include "rtweekend.h"
+
+#include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
-color ray_color(const ray& r, const hittable& world) {
-    hit_record rec;
-    if (world.hit(r, interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + color(1, 1, 1));
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(0.0, 0.0, 0.0) + a * color(0.3, 0.6, 1.0);
-}
-
 int main() {
-    // image
-
-    auto aspect_ratio = 16.0 / 9.0;
-    int  image_width  = 900;
-
-    // calc image_height and atleast 1
-
-    int image_height = int(image_width / aspect_ratio);
-    image_height = image_height < 1 ? 1 : image_height;
-
     // world
     // 2 sphere, 1 visible obj, 2nd ground, very large
 
     hittable_list world;
 
     shared_ptr<hittable> sphere1 = make_shared<sphere>(point3(0, 0, -1), 0.6);
-    shared_ptr<hittable> sphere2 = make_shared<sphere>(point3(0, -100.5, -1), 100);
+    shared_ptr<hittable> sphere2 = make_shared<sphere>(point3(0, -99.6, -1), 99);
     world.add(sphere1);
     world.add(sphere2);
 
-    // camera/eye
-    // viewport
-    // real 3d, 4th quad
-    // x-axis = right
-    // y-axis = up and inverted here to match with screen coords
-    // z-axis = towards me is positive, so viewport center is at (0, 0, -focal_length);
+    camera cam;
 
-    auto focal_length    = 1.0; 
-    auto viewport_height = 3.0;
-    auto viewport_width  = viewport_height * (double(image_width) / image_height);
-    auto camera_center   = point3(0, 0, 0);
+    cam.aspect_ratio  =  16.0  /  9.0;
+    cam.image_width   =  900;
 
-    // define horizontal & vertical vp vectors edges
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // define square-pixel delta unit
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // define vp top-left corner & pixel P(0, 0, 0);
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v  / 2;
-    auto pixel00_loc         = viewport_upper_left + (pixel_delta_u + pixel_delta_v) / 2;
-
-    // render
-
-    std::cout << "P3\n" << image_width<< ' ' << image_height << "\n255\n";
-
-    // this is the canvas/screen, so the coords are like 
-    // that with top-left corner is (0,0,0), as is (i,j,0);
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_center  = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            // ray_direction = ray_direction.unit_length();
-            ray r(camera_center, ray_direction);
-            
-            color pixel_color = ray_color(r, world);
-            // auto pixels = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\rdone.                                   \n";
+    cam.render(world);
 
     return 0;
 
